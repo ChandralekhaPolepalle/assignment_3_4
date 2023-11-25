47f93a69cbdbddedb4e4cf4e2f59b1556fc753f5
Chandralekha <pchandralekha99@gmail.com>
2023-11-24 19:56:30 -0500

    Created a page for cad to usd and vice versa conversion

---
+++ script.js
@@ -0,0 +1,28 @@
+const canadianInput = document.getElementById("canadian");
+const usInput = document.getElementById("us");
+
+
+const rate = 1.37;
+
+function convertCurrency() {
+
+    const cad = parseFloat(canadianInput.value);
+    const usd = parseFloat(usInput.value);
+
+    if (((isNaN(cad) || cad < 0 ) && (isNaN(usd)) || usd<0 )) {
+        alert("Please enter valid numeric values.");
+        return;
+    }
+
+    if (!isNaN(cad)) {
+        // Convert to USD
+        const convertedUSD = cad * rate;
+        // Display result in the USD input box
+        usInput.value = convertedUSD.toFixed(3);
+    } else if (!isNaN(usd)) {
+        // Convert to CAD
+        const convertedCAD = usd / rate;
+        // Display result in the CAD input box
+        canadianInput.value = convertedCAD.toFixed(3);
+    }
+}
